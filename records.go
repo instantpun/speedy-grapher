e948976c274ba9e0aab9a67a53999afc0a3438fe
Aaron C. Robinett <arobinett_consulting@gmail.com>
2022-10-19 21:23:18 -0400

    remove commented code

--- records.go
+++ records.go
@@ -1,19 +1,7 @@
-package main
-
-import (
-	
-)
+package speedygrapher
 
 type Record map[string]string
 
 func NewRecord() Record {
 	return Record{}
 }
-
-// func NewRecordWithFields(fmap FieldnameMap) Record {
-// 	r := Record{}
-// 	for i, val := FieldnameMap {
-// 		r[]
-// 	}
-// 	return record
-// }
